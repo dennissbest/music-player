428c18f8aedc21785706d8b46ec3d5d20c5e3813
dennissbest <bestds@vcu.edu>
2026-09-20 23:17:50 -0400

    Update song titles in songs array

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const progressContainer = document.getElementById('progress-container');
 const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 
-const songs = ['hey', 'summer', 'ukulele'];
+const songs = ['Repose', 'Songclave', 'Silksong'];
 let songIndex = 2;
 
 function loadSong(song) {
