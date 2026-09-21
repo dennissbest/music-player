5a954c922219b321b92050a0b8a5f8ec2045c29c
dennissbest <bestds@vcu.edu>
2026-09-20 23:09:37 -0400

    Add music player functionality in script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+const songs = ['hey', 'summer', 'ukulele'];
+let songIndex = 2;
+
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+
+loadSong(songs[songIndex]);
